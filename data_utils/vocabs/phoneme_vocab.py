57e213b8dc1e11155696d3936215e0c698badbcf
hieunghia-pat <nghiangh@uit.edu.vn>
2024-12-31 09:16:02 +0700

    updated the phoneme vocab

--- data_utils/vocabs/phoneme_vocab.py
+++ data_utils/vocabs/phoneme_vocab.py
@@ -183,7 +183,6 @@ class PhonemeVocabv2:
             "up", "ut", "uy", "uya", "uych",
             "uyên", "uyêt", "uyn", "uynh",
             "uyp", "uyt", "uyu",
-            
             "uach", "uai", "uan", "uang", "uanh", "uao", "uat", "uau", "uay",
             "uăc", "uăm", "uăn", "uăng", "uăp", "uăt", "uâc", "uât", "uoang",
             "ue", "uen", "ueo", "uet", "uên", "uêt", "uêu",
